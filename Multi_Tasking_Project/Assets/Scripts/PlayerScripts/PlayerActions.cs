adb09f6357aac629d469e6812c75315266384270
Will M. 3 <willmcb234@gmail.com>
2020-09-16 13:14:34 -0400

    Camera Movement
    Fixed camera movement clamps so that they work

--- Multi_Tasking_Project/Assets/Scripts/PlayerScripts/PlayerActions.cs
+++ Multi_Tasking_Project/Assets/Scripts/PlayerScripts/PlayerActions.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class PlayerActions : MonoBehaviour
 {
     #region Movement Control Stuff
-    public float X_Max, X_Min, Y_Max, Y_Min;
+    public float X_Max, X_Min, Y_Max, Y_Min,xPos,yPos,zPos;
     public Vector3 PlayerPos;
     #endregion
     #region Game States
@@ -26,7 +26,10 @@ public class PlayerActions : MonoBehaviour
     }
     void Update()
     {
- PlayerPos = transform.position;//sets players current position
+PlayerPos = transform.position;//sets players current position
+xPos = transform.position.x;
+yPos = transform.position.y;
+zPos = transform.position.z;
  ////player movement inputs if game is not fully paused////
  if(currentState == GameState.Active)
         {
@@ -41,7 +44,7 @@ public class PlayerActions : MonoBehaviour
             }//player moves right
             if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
             {
-                transform.Translate(0f, 1f, 0f);
+               transform.Translate(0f, 1f, 0f);
             }//player moves up
             if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
             {
@@ -55,19 +58,19 @@ public class PlayerActions : MonoBehaviour
  /////player position limits////
  if(PlayerPos.x > X_Max)
         {
-            PlayerPos.x = X_Max;
+            transform.position = new Vector3(X_Max, yPos, zPos);
         }//max amount the player can move right in the x-direction
  if(PlayerPos.x < X_Min)
         {
-            PlayerPos.x = X_Min;
+            transform.position = new Vector3(X_Min, yPos, zPos);
         }//max amount the player can move left in the x-direction
  if(PlayerPos.y > Y_Max)
         {
-            PlayerPos.y = Y_Max;
+            transform.position = new Vector3(xPos,Y_Max, zPos);
         }//max amount the player can move up in the y-direction
  if(PlayerPos.y < Y_Min)
         {
-            PlayerPos.y = Y_Min;
+            transform.position = new Vector3(xPos,Y_Min,zPos);
         }//max amount the player can move down in the y-direction
  ////Stuff that happens when the game is fully paused/////
  if(currentState == GameState.Paused)
